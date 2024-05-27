4d55546d1cc5b6270a08f7b72969f03f4f165489
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-05-27 08:58:43 -0300

    Add `Tests_Utils` with test cases for convert meters from internal.

---
+++ RevitTest.Samples/Tests_Utils.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using NUnit.Framework;
+using System;
+
+namespace RevitTest.Sample
+{
+    public class Tests_Utils
+    {
+        private const double Tolerance = 1.0e-6;
+
+        [TestCase(1.0, 0.3048)]
+        [TestCase(2.0, 0.6096)]
+        [TestCase(3.0, 0.9144)]
+        [TestCase(4.2, 1.28016)]
+        [TestCase(5.3, 1.61544)]
+        [TestCase(6.4, 1.95072)]
+        public void RevitTests_FromInternalUnits_Meters(double meters, double internalMeters)
+        {
+            var convert = UnitUtils.ConvertFromInternalUnits(meters, UnitTypeId.Meters);
+            Console.WriteLine($"{meters} to {convert}");
+            
+            var almostEqual = Math.Abs(internalMeters - convert) < Tolerance;
+            Assert.IsTrue(almostEqual);
+        }
+    }
+}
